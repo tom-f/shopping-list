7903d655eca4ef2529ca3ff4c644a4648bc961e6
Tom Fawssett <tom.fawssett@gmail.com>
2017-03-01 21:57:05 +0000

    initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default class App extends React.Component {
+    render() {
+        return <div style={{textAlign: 'center'}}>
+            <h1>Shopping list!</h1>
+            </div>
+
+    }
+}
